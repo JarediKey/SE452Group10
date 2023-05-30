f1861b14601a83a1f228fae7846f5ba15d035d97
Ayyub Jose <ajose3@depaul.edu>
2023-05-29 19:09:14 -0500

    Web controllers for teacher , student and greeting page

---
+++ src/main/java/com/group10/se452_g10/webcontroller/TeacherController.java
@@ -0,0 +1,46 @@
+package com.group10.se452_g10.webcontroller;
+
+import com.group10.se452_g10.account.Student;
+import com.group10.se452_g10.account.StudentRepo;
+import com.group10.se452_g10.account.Teacher;
+import com.group10.se452_g10.account.TeacherRepo;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/teachers")
+public class TeacherController {
+    @Autowired
+    private TeacherRepo repo;
+
+    @GetMapping
+    public String list(Model model, HttpSession session) {
+        model.addAttribute("teacher", repo.findAll());
+        if (session.getAttribute("teacher") == null) {
+            model.addAttribute("teacher", new Teacher());
+            model.addAttribute("btnAddOrModifyLabel", "Add");
+        } else {
+            model.addAttribute("student", session.getAttribute("teacher"));
+            model.addAttribute("btnAddOrModifyLabel", "Modify");
+        }
+        return "student/list";
+    }
+
+    @GetMapping("/edit/{firstname}")
+    public String get(@PathVariable("firstname") String name, Model model, HttpSession session) {
+        session.setAttribute("teacher", repo.findByFirstName(name));
+        return "redirect:/teachers";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id, Model model, HttpSession session) {
+        repo.deleteById(id);
+        return "redirect:/teacher";
+    }
+
+}
