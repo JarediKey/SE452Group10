de9defca8fb423cdc94f7dcdf51aa0fb82987249
Jared Wang <wy.jared@gmail.com>
2023-06-04 12:37:38 -0500

    Delete guardian info of student.

--- src/main/java/com/group10/se452_g10/account/Student.java
+++ src/main/java/com/group10/se452_g10/account/Student.java
@@ -2,30 +2,13 @@ package com.group10.se452_g10.account;
 
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity(name = "students")
 public class Student extends User {
-
-    protected String guardianName;
-    protected int guardianNumber;
-
-    public Student() {
-    }
-
-    public Student(String firstName, String lastName) {
-        super(firstName, lastName);
-    }
-
-    public void setGuardianNumber(int guardianNumber) {
-        this.guardianNumber = guardianNumber;
-    }
-
-    public void setGuardianName(String guardianName) {
-        this.guardianName = guardianName;
-    }
 }
