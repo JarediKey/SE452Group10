341b47d3b17feb2be22d03b9ca48513b87488c36
Jared Wang <wy.jared@gmail.com>
2023-06-04 17:42:46 -0500

    Add package-info for web controller.

--- src/main/java/com/group10/se452_g10/webcontroller/package-info.java
+++ src/main/java/com/group10/se452_g10/webcontroller/package-info.java
@@ -2,5 +2,17 @@ package com.group10.se452_g10.webcontroller;
 
 /**
  * In this package, contains web services and implementations.
- * @author Ayyub Jose
+ * @author Ayyub Jose, Jared Wang
+ *
+ * ========Jared Wang========Jared Wang========Jared Wang========
+ * Function implementation:
+ * 1.Add login page for 3 different roles and provide the button of operations which the role is authorized after login.
+ * 2. For authority(ADMID)
+ *  -2.1 Add list course/student/teacher page with operation update(jump to edit page) and delete(delete item in page).
+ *  -2.2.Add craete/edit course page.
+ *  -2.3.Add create/edit page of student/teacher.(save with encode password, edit page will never show previous password.)
+ * 3. For authority(STUDENT, TEACHER)
+ *  -3.1 Add course search page and search result. (Type keyword in search-page, jump to search-result show the reulst list)
+ * ========Jared Wang========Jared Wang========Jared Wang========
+ *
  */
